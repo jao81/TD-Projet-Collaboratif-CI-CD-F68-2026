c1463e56430a32e421cfd7f077384ce152fb9437
PJ <2582039@csfoy.ca>
2026-09-23 20:05:24 -0400

    Team B main changes and Predict Integrations test modifications

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-<<<<<<< HEAD
 from app.utils import predict
 from fastapi import FastAPI, HTTPException
-=======
 
 from app.utils import predict, predict_nv_model
->>>>>>> 0bb91fb6e17146876d4942ee369e15e7c8263eae
 
 app = FastAPI()
 
@@ -15,6 +12,8 @@ class PredictionRequest(BaseModel):
 
 @app.post("/predict")
 def predict_endpoint(data: PredictionRequest):
+    if not data.features: 
+        raise HTTPException( status_code=400, detail="features must contain at least one value", ) 
     predictions = predict(data.features)
     return {
         "model_version": "v1",
@@ -29,20 +28,13 @@ def read_root():
 def favicon():
     return ""
 
-<<<<<<< HEAD
-@app.post("/predict") 
-def predict_endpoint(data: PredictionRequest): 
-    if not data.features: 
-        raise HTTPException( status_code=400, detail="features must contain at least one value", ) 
-    predictions = predict(data.features) 
-    return {"predictions": predictions}
-=======
 @app.post("/predictNvModel")
 def predict_endpoint_v2(data: PredictionRequest):
+    if not data.features: 
+        raise HTTPException( status_code=400, detail="features must contain at least one value", )     
     predictions = predict_nv_model(data.features)
 
     return {
         "model_version": "v2",
         "predictions": predictions
     }
->>>>>>> 0bb91fb6e17146876d4942ee369e15e7c8263eae
